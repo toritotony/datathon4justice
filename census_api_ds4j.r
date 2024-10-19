a2a8cf205a2812ab3eeaf4d68ebbc79026d92044
toritotony <wolfeanthony584@gmail.com>
2024-10-19 11:22:44 -0700

    Added more code to assist me in future analysis

--- census_api_ds4j.r
+++ census_api_ds4j.r
@@ -62,8 +62,31 @@ totalpop <- map_df(us, function(x) {
 
 
 # variable changes 2023 1 year for API
-
 excelvars2023 <- read_excel("C:\\Users\\wolfe\\Downloads\\2023-1yr-api-changes.csv")
 
 
+# get median age by state in 2020 with data drawn from demographic characteristics, then plot ggplot
+age20 <- get_decennial(geography = "state", 
+                       variables = "P13_001N", 
+                       year = 2020,
+                       sumfile = "dhc")
+age20 %>%
+  ggplot(aes(x = value, y = reorder(NAME, value))) + 
+  geom_point()
 
+
+# get median household income data from 2017-2021 ACS for Vermont then plot point + uncertainty
+vt <- get_acs(geography = "county", 
+              variables = c(medincome = "B19013_001"), 
+              state = "VT", 
+              year = 2021)
+
+vt %>%
+  mutate(NAME = gsub(" County, Vermont", "", NAME)) %>%
+  ggplot(aes(x = estimate, y = reorder(NAME, estimate))) +
+  geom_errorbarh(aes(xmin = estimate - moe, xmax = estimate + moe)) +
+  geom_point(color = "red", size = 3) +
+  labs(title = "Household income by county in Vermont",
+       subtitle = "2017-2021 American Community Survey",
+       y = "",
+       x = "ACS estimate (bars represent margin of error)")
